6d66d293a808ad638d5b79003a42a54477470169
Ben Marsh <bmarsh94@gmail.com>
2016-10-29 23:43:16 +0200

    Remove the folder. Don't need it

--- Set 1/challenge1.py
+++
@@ -1,7 +0,0 @@
-# cryptopals challenge 1
-import binascii
-
-orig = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-hex = orig.decode("hex")
-out = binascii.b2a_base64(hex)
-print(out)
--- Set 1/challenge2.py
+++
@@ -1,5 +0,0 @@
-# Cryptopals challenge 2
-
-string = int("1c0111001f010100061a024b53535009181c", 16)
-key = int("686974207468652062756c6c277320657965", 16)
-print "%X" % (string^key)
