9db0233dfcacbbac21ed8c6d03a9bff1de83803a
Ben Marsh <bmarsh94@gmail.com>
2017-07-12 21:41:24 +0100

    General cleanup of challenge3

--- challenge3.py
+++ challenge3.py
@@ -3,6 +3,7 @@
 import array
 import re
 
+#most common english letters
 letter_points = {
     'e' : 10,
     't' : 10,
@@ -18,11 +19,13 @@ letter_points = {
     'u' : 5
 }
 
+
 def decode(orig, char):
     hex_data = orig.decode('hex')
     byte_arr = array.array('b', hex_data)
     result = [b ^ char for b in byte_arr]
     return "".join("{:c}".format(b) for b in result)
+
 
 def score(string):
     score = 0
@@ -30,6 +33,7 @@ def score(string):
         if x in letter_points:
             score += letter_points[x]
     return score
+
 
 def get_decrypted(orig):
     most_likely_mess = ""
@@ -45,6 +49,7 @@ def get_decrypted(orig):
             
     print (most_likely_mess)
     print chr(most_likely_key)
+
 
 encoded = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 get_decrypted(encoded)
