697d6aa63ed391fc324cb0b08717ac8794362c50
Ben Marsh <bmarsh94@gmail.com>
2017-07-25 15:09:31 +0100

    Add the ability to use a cloning state to MT19937

--- challenge21.py
+++ challenge21.py
@@ -5,21 +5,29 @@
 
 class MT19937:
 
-    def __init__(self, seed):
+    def __init__(self, seed, clonestate=None):
         self.index = 624
         self.mt = [0] * 624
-        self.mt[0] = seed
-        for i in range(1, 624):
-            self.mt[i] = (0x6c078965 * (self.mt[i - 1] ^ self.mt[i - 1] >> 30) + i) & 0xffffffff
+        
+        if clonestate is None:
+            self.mt[0] = seed
+            for i in range(1, 624):
+                self.mt[i] = (0x6c078965 * (self.mt[i - 1] ^ self.mt[i - 1] >> 30) + i) & 0xffffffff
 
+        else:
+            self.mt = clonestate
+            self.index = 0
 
     def extract(self):
         if self.index >= 624:
             self.twist()
-
+            
         y = self.mt[self.index]
+        #constants used for bitshits and bitwise operations from wikipedia
         y = y ^ y >> 11
+        #0x9d2c5680 = 2636928640
         y = y ^ y << 7 & 0x9d2c5680
+        #0xefc60000 = 4022730752
         y = y ^ y << 15 & 0xefc60000
         y = y ^ y >> 18
 
@@ -41,5 +49,5 @@ class MT19937:
 if __name__ == '__main__':
     
     prng = MT19937(2017)
-    for i in range(50):
+    for i in range(5):
         print(prng.extract())
