9896b7cb61bd456b8f907cb5737720e34b3698bd
Ben Marsh <bmarsh94@gmail.com>
2018-01-04 21:54:23 +0000

    Initial commit for challenge 43

--- challenge43.py
+++ challenge43.py
@@ -1,1 +1,19 @@
 #cryptopals challenge 43
+
+from Crypto.Random import random
+from Crypto.Util.number import getPrime
+import binascii
+import hashlib
+
+def egcd(a, b):
+    if b == 0:
+        return (1, 0)
+    else:
+        q = a // b;
+        r = a % b;
+        (s, t) = egcd(b, r)
+        return (t, s - q * t)
+
+def invmod(a, N):
+    (x, y) = egcd(a, N)
+    return x % N
